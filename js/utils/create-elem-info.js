354065dda894ae9aa5515011e237878cf9b54788
Maxim-Belyi <btf9458@gmail.com>
2026-08-22 11:21:53 +0500

    fix(): popup style fix

--- js/utils/create-elem-info.js
+++ js/utils/create-elem-info.js
@@ -1,9 +1,11 @@
 import { getCoords } from "./get-coords.js";
 
 export function createElementInfo(element) {
+    const w = element.clientWidth;
+    const h = element.clientHeight;
     return {
-      width: element.clientWidth,
-      height: element.clientHeight,
+      width: w > 0 ? w : 50,
+      height: h > 0 ? h : 100,
       coords: getCoords(element),
       visible: true,
     };
